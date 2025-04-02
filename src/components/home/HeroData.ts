85b5cdfa1527bc1117a7e134c41a4ca8242f8099
M9china <nqobilencaza2@gmail.com>
2025-04-02 12:06:36 +0200

    add HeroData component and update package.json with react-slick and slick-carousel dependencies

---
+++ src/components/home/HeroData.ts
@@ -0,0 +1,22 @@
+
+  export const slides = [
+    {
+      src: "/hero/zembelani (8).jpeg",
+      alt: "Two models wearing women's black cotton crewneck tee and off-white cotton crewneck tee.",
+      title: "New Arrivals",
+      description: "Shop now",
+    },
+    {
+      src: "/hero/zembelani (9).jpeg",
+      alt: "Wooden shelf with gray and olive drab green baseball caps, next to wooden clothes hanger with sweaters.",
+      title: "Best Selling",
+      description: "Shop now",
+    },
+    {
+      src: "/hero/zembelani (4).jpeg",
+      alt: "Walnut desk organizer set with white modular trays, next to porcelain mug on wooden desk.",
+      title: "Head Wear",
+      description: "Shop now",
+    },
+  ]
+  
