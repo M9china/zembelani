8ecd663a3a3057fc1a584acc3c8d5de1fccb4178
m9china <nqobilencaza2@gmail.com>
2025-07-08 11:33:19 +0200

    refactor(ProjectDetailPage): simplify component structure and enhance project detail rendering

--- src/app/portfolio/[id]/page.tsx
+++ src/app/portfolio/[id]/page.tsx
@@ -1,26 +1,32 @@
 import { ProjectsStatic } from "@/components";
+import { notFound } from "next/navigation";
+import Image from "next/image";
 
-interface Props {
-    params: {
-        id: string;
-    };
-}
+export default async function ProjectDetailPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const projectId = await params
+    .then((p) => parseInt(p.id, 10))
+    .catch(() => null);
+  if (!projectId) return notFound();
 
-export default function DetailsPage({ params }: Props) {
-    const projectId = parseInt(params.id);
-    const project = ProjectsStatic.find((proj) => proj.id === projectId);
-    return (
-        <main className="container mx-auto p-4 pt-[5rem] bg-white text-black">
-            <h1>Project Details</h1>
-            <p>This is the details page for a specific project.</p>
-            { project ? (
-                <div>
-                    <h2>{project.title}</h2>
-                    <p>{project.description}</p>
-                </div>
-            ) : (
-                <p>Project not found.</p>
-            )}
-        </main>
-    );
+  const project = ProjectsStatic.find((p) => p.id === projectId);
+
+  return (
+    <main className="min-h-screen pt-[5rem] p-8 bg-white">
+      <h1 className="text-3xl font-bold text-[#001A4B] mb-6">
+        {project?.title}
+      </h1>
+      <Image
+      src={project?.imageUrl || "/projects/default.png"}
+        alt={project?.title || "Project Image"}
+        width={1200}
+        height={600}
+        className="w-full h-[20rem] object-cover rounded-2xl mb-4"
+      />
+      <p className="text-gray-800 text-lg">{project?.description}</p>
+    </main>
+  );
 }
