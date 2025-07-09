c5f73c3acb15dd7369cb093ee63474b00df37a51
m9china <nqobilencaza2@gmail.com>
2025-07-09 10:46:58 +0200

    style(Portfolio): adjust button padding and update card border radius for consistency

--- src/components/projects/Portfolio.tsx
+++ src/components/projects/Portfolio.tsx
@@ -14,7 +14,7 @@ export const Portfolio = () => {
           Below is a selection of projects we have worked on:
         </p>
         <Link
-          className="text-white bg-blue-900 text-xs font-bold py-2 px-4 rounded"
+          className="text-white bg-blue-900 text-xs font-bold py-3 px-4 rounded"
           href="/contact-us"
         >
           CONTACT US NOW
@@ -22,19 +22,19 @@ export const Portfolio = () => {
       </div>
       <div className="mt-16 ">
         {ProjectsStatic.map((project, id) => (
-          <div className="mx-4 mb-12 rounded-2xl shadow-lg shadow-[#001A4B]" key={id}>
+          <div className="mx-4 mb-12 rounded-lg shadow-lg shadow-[#001A4B]" key={id}>
             <Link
               href={`/portfolio/${project.id}`}
               className="shadow-2xl shadow-gray-300  "
             >
               <Image
-                className=" h-[11rem] rounded-t-2xl object-cover w-full brightness-50"
+                className=" h-[11rem] rounded-t-lg object-cover w-full brightness-50"
                 alt={project.title}
                 width={1920}
                 height={1080}
                 src={project.imageUrl[0]}
               />
-              <p className="text-white bg-[#001A4B] rounded-b-2xl font-bold py-6 px-4 text-2xl">
+              <p className="text-white bg-[#001A4B] rounded-b-lg font-bold py-6 px-4 text-2xl">
                 {" "}
                 {project.description}
               </p>
