caac4f13cf8bdb15b5bae7f575bd57e9c5c8ae98
m9china <nqobilencaza2@gmail.com>
2025-07-10 16:02:13 +0200

    style(OurServices): adjust heading margin and reposition services description for improved layout

--- src/components/our-services/OurServices.tsx
+++ src/components/our-services/OurServices.tsx
@@ -4,10 +4,11 @@ import Image from "next/image";
 export const OurServices = () => {
   return (
     <main className="bg-gray-50 min-h-screen pt-[7rem]">
-      <h1 className="text-4xl gap-2 px-4 font-bold text-[#001A4B] mb-12">
+      <h1 className="text-4xl gap-2 px-4 font-bold text-[#001A4B] mb-4">
         Our <br />
         Services
       </h1>
+      <p className="text-gray-800 text-lg px-4">Our services include but not limited to the following:</p>
       <div className="px-4 space-y-4 pb-16">
         {ServiceStatic.map((item, index) => (
           <div key={index} className="mt-12">
