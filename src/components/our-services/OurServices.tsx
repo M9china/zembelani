6856db53763541c475bb36a3cd7b2fce6d69e31a
m9china <nqobilencaza2@gmail.com>
2025-07-10 15:58:39 +0200

    feat(OurServices): update service offerings with new images and descriptions; enhance layout and styling

--- src/components/our-services/OurServices.tsx
+++ src/components/our-services/OurServices.tsx
@@ -3,23 +3,23 @@ import Image from "next/image";
 
 export const OurServices = () => {
   return (
-    <main className="bg-gray-50 min-h-screen pt-[6rem]">
-      <h1 className="text-4xl gap-2 px-4 font-bold text-[#001A4B] mb-8">
+    <main className="bg-gray-50 min-h-screen pt-[7rem]">
+      <h1 className="text-4xl gap-2 px-4 font-bold text-[#001A4B] mb-12">
         Our <br />
         Services
       </h1>
-      <div className="px-8 space-y-4">
+      <div className="px-4 space-y-4 pb-16">
         {ServiceStatic.map((item, index) => (
-          <div key={index} className="">
+          <div key={index} className="mt-12">
             <Image
-              className=""
+              className="object-cover rounded w-full h-[14rem]"
               width={1080}
               height={1920}
               src={item.imageUrl || "/default"}
               alt={"our-machinery"}
             />
-            <h1 className="text-2xl font-bold text-[#001A4B]">{item.title}</h1>
-            <p className="text-gray-600">{item.description}</p>
+            <h1 className="text-2xl font-bold text-[#001A4B] mt-10">{item.title}</h1>
+            <p className="text-gray-800 text-lg mt-2">{item.description}</p>
           </div>
         ))}
       </div>
--- src/components/our-services/ServiceStatic.ts
+++ src/components/our-services/ServiceStatic.ts
@@ -5,23 +5,28 @@ export declare interface IServiceStatic {
 }
 export const ServiceStatic: IServiceStatic[] = [
   {
-    title: "Web Development",
-    description: "Building responsive and dynamic websites.",
-    imageUrl: "/",
+    title: "BUS HIRE SERVICES",
+    description: `Our fleet of roadworthy busses are operated by qualified and experienced drivers.`,
+    imageUrl: "/projects/busses.jpg",
   },
   {
-    title: "Mobile App Development",
-    description: "Creating user-friendly mobile applications.",
-    imageUrl: "/",
+    title: "CONSTRUCTION SERVICES",
+    description: `We have professional and qualified engineers, project managers and safety officers for every project acquired.`,
+    imageUrl: "/services/construct.jpg",
   },
   {
-    title: "UI/UX Design",
-    description: "Designing intuitive user interfaces and experiences.",
-    imageUrl: "/",
+    title: "PLANT HIRE SERVICES",
+    description: `We have a wide range of plant and machinery available for hire, including excavators, bulldozers, and more.`,
+    imageUrl: "/services/excavator.jpg",
   },
   {
-    title: "Digital Marketing",
-    description: "Promoting brands through digital channels.",
-    imageUrl: "/",
+    title: "TRUCK HIRE SERVICES",
+    description: `We have a fleet of well-maintained trucks available for hire, suitable for various transportation needs.`,
+    imageUrl: "/services/truckhire.jpg",
   },
+  {
+title: "MATERIAL SUPPLY SERVICES",
+description: `We supply a wide range of construction materials, including aggregates, asphalt, concrete, etc.`,
+imageUrl: "/services/material.jpg",
+  }
 ];
